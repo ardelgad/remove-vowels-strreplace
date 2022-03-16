b4f6e71821819fca17cefccb1e73a2406038d0f6
ardelgad <adelgadoporras@gmail.com>
2022-03-16 10:13:22 +0100

    terminado

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 function removeVowels(frase) {
     // Implementar código aquí
+    /*Ejercicio 1
+    Implementa la función __removeVowels__ del fichero remove-vowels.js. Verifica que tu algoritmo funciona bien ejecutando __node app.js__ */
+    let vowels = /[aeiou]/gi
    
     // Obligatorio: utilizar una expresión regular y el método de string "replace": https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/replace
 
-    let resultado = "IMPLEMENTAME!!";
+    let resultado = frase.replace(vowels, "")
 
     return resultado;
 }
 console.log(removeVowels('codewars')); // --> cdwrs
 console.log(removeVowels('holaholaaa')); // -->hlhl
+
+
+
+/*Ejercicio 2
+
+Instala Jest. Modifica el fichero package para poder ejecutar los tests. Escribe dos tests que prueben un conjunto de datos suficiente para la función de eliminar vocales. */
